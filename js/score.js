2c2366aa6f13ac06f234b3b7a3a2b8faa238d455
Camilo García <72902253+camilogs1@users.noreply.github.com>
2022-05-04 00:50:24 -0500

    Update

--- js/score.js
+++ js/score.js
@@ -1,7 +1,7 @@
+var score = 0
+
 function score_page2()
 {
-    var score = 0
-
     //Pregunta 1.12
     if(document.getElementById("pre_1.12_a").checked)
     {
@@ -33,5 +33,115 @@ function score_page2()
     {
         score += 3
     }
-    
+}
+
+function score_page3()
+{
+    //Pregunta 3.1
+    if(document.getElementById("pre_3.1_a").checked)
+    {
+        score += 2
+    }
+    ifelse(document.getElementById("pre_3.1_b").checked)
+    {
+        score += 1
+    }
+
+    //Pregunta 3.2
+    if(document.getElementById("pre_3.2_a").checked)
+    {
+        score += 2
+    }
+    ifelse(document.getElementById("pre_3.2_b").checked)
+    {
+        score += 1
+    }
+    ifelse(document.getElementById("pre_3.2_c").checked)
+    {
+        score += 2
+    }
+
+    //Pregunta 4.1
+    if(document.getElementById("pre_4.1_a").checked)
+    {
+        score += 0
+    }
+    ifelse(document.getElementById("pre_4.1_b").checked)
+    {
+        score += 1
+    }
+    ifelse(document.getElementById("pre_4.1_c").checked)
+    {
+        score += 2
+    }
+    ifelse(document.getElementById("pre_4.1_d").checked)
+    {   
+        score += 2
+    }
+    ifelse(document.getElementById("pre_4.1_e").checked)
+    {
+        score += 3
+    }
+    ifelse(document.getElementById("pre_4.1_f").checked)
+    {
+        score += 3
+    }
+    ifelse(document.getElementById("pre_4.1_g").checked)
+    {
+        score += 2
+    }
+    ifelse(document.getElementById("pre_4.1_h").checked)
+    {
+        score += 4
+    }
+    ifelse(document.getElementById("pre_4.1_i").checked)
+    {
+        score += 2
+    }
+    ifelse(document.getElementById("pre_4.1_j").checked)
+    {
+        score += 0
+    }
+
+    //Pregunta 4.2.1
+    if(document.getElementById("pre_4.2.1_a").checked)
+    {
+        score += 0
+    }
+    ifelse(document.getElementById("pre_4.2.1_b").checked)
+    {
+        score += 1
+    }
+    ifelse(document.getElementById("pre_4.2.1_c").checked)
+    {
+        score += 2
+    }
+    ifelse(document.getElementById("pre_4.2.1_d").checked)
+    {   
+        score += 2
+    }
+    ifelse(document.getElementById("pre_4.2.1_e").checked)
+    {
+        score += 3
+    }
+    ifelse(document.getElementById("pre_4.2.1_f").checked)
+    {
+        score += 3
+    }
+    ifelse(document.getElementById("pre_4.2.1_g").checked)
+    {
+        score += 2
+    }
+    ifelse(document.getElementById("pre_4.2.1_h").checked)
+    {
+        score += 4
+    }
+    ifelse(document.getElementById("pre_4.2.1_i").checked)
+    {
+        score += 2
+    }
+    ifelse(document.getElementById("pre_4.2.1_j").checked)
+    {
+        score += 0
+    }
 }
